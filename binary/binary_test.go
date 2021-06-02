70c6a4fafaa0612d547726ae50f96a65b6d64835
dingxiuan <dingxiuan@163.com>
2021-06-02 17:04:26 +0800

    测试

--- binary/binary_test.go
+++ binary/binary_test.go
@@ -1,5 +1,19 @@
 package binary
 
-//func TestBinary(t *testing.T) {
-//	test()
-//}
+import (
+	"testing"
+	"time"
+)
+
+// 测试二进制工具
+func TestBinary(t *testing.T) {
+	//bytes := Int32ToBytesBE(365536, 4)
+	now := time.Now()
+	bytes := Int64ToBytesBE(now.Unix(), 4)
+	println("bytes: ", BytesToHex(bytes, false))
+	println("bytes2: ", BytesToSplitHex(bytes, false, " ", 1))
+	println("value: ", BytesToInt32BE(bytes, false))
+	println("binary: ", BytesToBinary(bytes, " ", 1))
+	println("now: ", now.Format("2006-01-02 15:04:05"))
+	println("now: ", now.Unix())
+}
--- main.go
+++ main.go
@@ -1,26 +1,4 @@
 package main
 
-import (
-	"github.com/benefitj/benefitgo-commons/binary"
-	"time"
-)
-
 func main() {
-
-	testBinaryTool()
-
-	UUID()
-}
-
-// 测试二进制工具
-func testBinaryTool() {
-	//bytes := binary.Int32ToBytesBE(365536, 4)
-	now := time.Now()
-	bytes := binary.Int64ToBytesBE(-now.Unix(), 4)
-	println("bytes: ", binary.BytesToHex(bytes, false))
-	println("bytes2: ", binary.BytesToSplitHex(bytes, false, " ", 1))
-	println("value: ", binary.BytesToInt32BE(bytes, false))
-	println("binary: ", binary.BytesToBinary(bytes, " ", 1))
-	println("now: ", now.Format("2006-01-02 15:04:05"))
-	println("now: ", now.Unix())
 }
